be29434671bf87c23041ba241328b36a393ba2b5
OliV <goffygoo@github.com>
2024-06-23 21:19:27 +0530

    minor changes

--- service/dashboard/query/queries/createAppConfig.js
+++ service/dashboard/query/queries/createAppConfig.js
@@ -2,21 +2,12 @@ import { DataTypes } from "../../../../constants/index.js";
 import AppConfig from "../../../../model/AppConfig.js";
 
 const query = async params => {
-    await AppConfig.create({
-        city: "delhi",
-        key: "topEvents",
-        data: []
-    });
-    await AppConfig.create({
-        city: "delhi",
-        key: "topVenues",
-        data: []
-    });
+    return AppConfig.find({});
 }
 
 export default {
     query,
-    title: "Init App config",
+    title: "show App config",
     params: {
         id: DataTypes.string,
     }
