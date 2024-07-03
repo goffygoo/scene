b84d98e51c4f6cf18b3ebeccd51c9a7500fa1e05
OliV <goffygoo@github.com>
2024-07-04 00:12:27 +0530

    email

--- service/comms/mail/index.js
+++ service/comms/mail/index.js
@@ -5,17 +5,20 @@ import config from "../../../constants/config.js";
 const { MAILER_ID, MAILER_PASSWORD } = config;
 
 let transporter = nodemailer.createTransport({
-    service: "gmail",
+    service: 'Godaddy',
+    host: "smtpout.secureserver.net",  
+    secureConnection: true,
+    port: 465,
     auth: {
         user: MAILER_ID,
-        pass: MAILER_PASSWORD,
-    },
+        pass: MAILER_PASSWORD
+    }
 });
 
 const sendMailToUser = async (email, subject, html) => {
     try {
         let mailOptions = {
-            from: `"Kul Bois" <${MAILER_ID}>`,
+            from: `"Scene" <${MAILER_ID}>`,
             to: email,
             subject,
             html,
@@ -28,7 +31,7 @@ const sendOtpMail = async (email, otp) => {
     const template = otpTemplate(otp);
     return sendMailToUser(
         email,
-        'OTP for BaljeetKode login',
+        'OTP for Scene login',
         template
     );
 }
@@ -37,7 +40,7 @@ const sendOtpResetPassword = async (email, otp) => {
     const template = otpResetPasswordTemplate(otp);
     return sendMailToUser(
         email,
-        'OTP for BaljeetKode Password Reset',
+        'OTP for Scene Password Reset',
         template
     );
 }
