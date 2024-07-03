b1e6f244150cc6ef27cada2b96ef05fde07e969b
OliV <goffygoo@github.com>
2024-07-04 00:14:46 +0530

    config

--- scripts/copyConfig.sh
+++ scripts/copyConfig.sh
@@ -1,8 +1,8 @@
 cp ../vault/config.js ./constants/
 cp ../vault/google-application-creds.json ./constants/
+cp ../vault/.env ./
 mkdir cert
 cp ../vault/cert/myCA.pem ./cert/
 cp ../vault/cert/baljeetkode.ca.crt ./cert/
 cp ../vault/cert/baljeetkode.certificate.crt ./cert/
 cp ../vault/cert/baljeetkode.private.key ./cert/
-cp ../vault/.env ./
