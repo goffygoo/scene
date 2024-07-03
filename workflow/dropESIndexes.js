31b7afac150678e7dfe6917f51a75399f70bdb50
OliV <goffygoo@github.com>
2024-07-03 22:44:50 +0530

    logs

--- workflow/dropESIndexes.js
+++ workflow/dropESIndexes.js
@@ -2,11 +2,12 @@ import _db from '../util/db.js';
 import LogModule from "../service/log/index.js";
 
 const execute = async (params) => {
-    const { logStamp, eventStamp, errorStamp } = params;
+    const { logStamp, eventStamp, errorStamp, feLogStamp } = params;
     await LogModule.dropIndexes({
         errorStamp,
         eventStamp,
-        logStamp
+        logStamp,
+        feLogStamp,
     });
 }
 
