4be45d8afe90a1321ef3f890c73698cf5143be33
OliV <goffygoo@github.com>
2024-07-04 23:59:00 +0530

    logs

--- service/init.js
+++ service/init.js
@@ -3,11 +3,12 @@ import DashboardModule from "./dashboard/index.js";
 import LogModule from "./log/index.js";
 
 const createIndexes = async () => {
+    console.log("index created");
     await LogModule.createIndexes(true);
 }
 
 const initScheduler = () => {
-    cron.schedule("55 23 * * *", createIndexes);
+    cron.schedule("1 0 * * *", createIndexes);
     cron.schedule("*/2 * * * *", DashboardModule.feature.pollFeatureConfig);
     cron.schedule("*/5 * * * *", DashboardModule.appConfig.pollAppConfig);
 }
