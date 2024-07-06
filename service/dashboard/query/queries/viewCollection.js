6860ab2c593230b4f24fa861296d0fc138ed29a3
OliV <goffygoo@github.com>
2024-07-06 20:49:14 +0530

    query

---
+++ service/dashboard/query/queries/viewCollection.js
@@ -0,0 +1,13 @@
+import db from "../../../../util/db.js";
+
+const query = async (params) => {
+    const { collection } = params;
+    const data = await db.collection(collection).find().toArray();
+    return data;
+};
+
+export default {
+    query,
+    title: "View content of collection",
+    params: { collection: DataTypes.string },
+};
