f8aa1e85ed9e9871ef8ec1051b88ab9571ecd868
OliV <goffygoo@github.com>
2024-07-17 23:25:47 +0530

    cleanup

--- service/dashboard/query/queries/addOrganiser.js
+++ service/dashboard/query/queries/addOrganiser.js
@@ -1,13 +1,12 @@
 import { DataTypes } from "../../../../constants/index.js";
 import Organiser from "../../../../model/Organiser.js";
-import { processPassword } from "../../../auth/utils.js";
+import AuthModule from "../../../auth/index.js";
 
 const query = async (params) => {
   const { email, password } = params;
-
   const data = await Organiser.create({
     email,
-    password: processPassword(password),
+    password: AuthModule.processPassword(password),
   });
   return data;
 };
@@ -15,5 +14,8 @@ const query = async (params) => {
 export default {
   query,
   title: "Add Organiser Account",
-  params: { email: DataTypes.string, password: DataTypes.string },
+  params: {
+    email: DataTypes.string,
+    password: DataTypes.string
+  },
 };
