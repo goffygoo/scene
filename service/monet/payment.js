ca6077b1e92063249f15ef605c11d543d92bba41
OliV <goffygoo@github.com>
2024-07-20 16:23:02 +0530

    logs

--- service/monet/payment.js
+++ service/monet/payment.js
@@ -1,13 +1,27 @@
 import Razorpay from "razorpay";
 import config from "../../constants/config.js";
+import LogModule from "../log/index.js";
+import asyncLocalStorage from "../../util/asyncStorage.js";
 
 const PaymentInstance = new Razorpay({
 	key_id: config.RAZORPAY_KEY_ID,
 	key_secret: config.RAZORPAY_KEY_SECRET,
 });
 
+const log = async (data, key2, metric) => {
+	const txnId = asyncLocalStorage.getStore();
+	LogModule.log({
+		data: JSON.stringify(data),
+		key1: 'pgQueries',
+		key2,
+		txnId,
+		metric,
+	})
+}
+
 const createPaymentOrder = async ({ userId, orderId, amount }) => {
-	return PaymentInstance.orders.create({
+	const startTime = Date.now();
+	const result = await PaymentInstance.orders.create({
 		amount: +amount * 100,
 		currency: "INR",
 		notes: {
@@ -15,22 +29,33 @@ const createPaymentOrder = async ({ userId, orderId, amount }) => {
 			orderId,
 		},
 	});
+	log({ data: { userId, orderId, amount }, result }, 'createPaymentOrder', Date.now() - startTime);
+	return result;
 };
 
 const getPaymentOrder = async (orderId) => {
-	return PaymentInstance.orders.fetch(orderId);
+	const startTime = Date.now();
+	const result = await PaymentInstance.orders.fetch(orderId);
+	log({ orderId, result }, 'getPaymentOrder', Date.now() - startTime);
+	return result;
 };
 
 const checkPaymentStatus = async (paymentId) => {
-	return PaymentInstance.payments.fetch(paymentId);
+	const startTime = Date.now();
+	const result = await PaymentInstance.payments.fetch(paymentId);
+	log({ paymentId, result }, 'checkPaymentStatus', Date.now() - startTime);
+	return result;
 };
 
 const refundPayment = async (paymentId) => {
-	return PaymentInstance.payments.refund(paymentId)
+	const startTime = Date.now();
+	const result = PaymentInstance.payments.refund(paymentId);
+	log({ paymentId, result }, 'refundPayment', Date.now() - startTime);
+	return result;
 };
 
 export default {
-    createPaymentOrder,
+	createPaymentOrder,
 	checkPaymentStatus,
 	getPaymentOrder,
 	refundPayment,
